a9b09863fb61bb90f950b16228ae119fd65da818
Marcio Souza Filho <marciorasf@gmail.com>
2020-08-15 04:15:08 -0300

    add category routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 
+import CategoryController from "./controllers/CategoryController";
 import FlashcardController from "./controllers/FlashcardController";
 import UserController from "./controllers/UserController";
 import Auth from "./middlewares/Auth";
@@ -9,10 +10,14 @@ const routes = express.Router();
 routes.post("/user", UserController.create);
 routes.post("/authenticate", UserController.authenticate);
 
-routes.put("/flashcard", Auth, FlashcardController.update);
+routes.post("/flashcard", Auth, FlashcardController.create);
 routes.get("/flashcard", Auth, FlashcardController.getById);
 routes.get("/flashcards", Auth, FlashcardController.index);
 routes.get("/flashcard/random", Auth, FlashcardController.getRandom);
-routes.post("/flashcard", Auth, FlashcardController.create);
+routes.put("/flashcard", Auth, FlashcardController.update);
+
+routes.post("/category", Auth, CategoryController.create);
+routes.get("/categories", Auth, CategoryController.index);
+routes.put("/category", Auth, CategoryController.update);
 
 export default routes;
