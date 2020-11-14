6d4a466be873aeb53d0f9bd873444d2ffa6903a8
marciorasf <marciorasf@gmail.com>
2020-11-13 21:54:23 -0300

    udpate env variables

--- src/config.ts
+++ src/config.ts
@@ -7,8 +7,4 @@ export const salt_rounds = process.env.SALT_ROUNDS;
 export const email_user = process.env.EMAIL_USER;
 export const email_password = process.env.EMAIL_PASSWORD;
 
-export const db_name = process.env.DB_NAME;
-export const db_user = process.env.DB_USER;
-export const db_password = process.env.DB_PASSWORD;
-
 export const web_url = process.env.WEB_URL;
