d51236a59ef1be3a08699ba609dbfaa59f72e7e4
marciorasf <marciorasf@gmail.com>
2020-11-17 21:29:10 -0300

    update containers names

--- scripts/restore-dev-db.sh
+++ scripts/restore-dev-db.sh
@@ -1,2 +1,2 @@
-sudo docker exec flashcards_api npx ts-node ./src/database/clean.ts
-sudo docker exec flashcards_api npx ts-node ./src/database/populate.ts
+sudo docker exec flashcards-api npx ts-node ./src/database/clean.ts
+sudo docker exec flashcards-api npx ts-node ./src/database/populate.ts
