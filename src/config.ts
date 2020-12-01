10946ccc6040e3ff73ce5b38852630cd9157baa3
marciorasf <marciorasf@gmail.com>
2020-12-01 20:35:18 -0300

    get mail api_key from environment

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,6 @@ export const port = process.env.PORT;
 export const secret = process.env.SECRET as string;
 export const salt_rounds = process.env.SALT_ROUNDS;
 
-export const email_api_user = process.env.EMAIL_API_USER;
 export const email_api_key = process.env.EMAIL_API_KEY;
 export const email_from = process.env.EMAIL_FROM;
 
--- src/services/MailService.ts
+++ src/services/MailService.ts
@@ -1,12 +1,12 @@
 import * as mailer from "nodemailer";
 import * as sgTransport from "nodemailer-sendgrid-transport";
 
-import { email_from, email_api_user, email_api_key, web_url } from "../config";
+import { email_from, email_api_key, web_url } from "../config";
 
 class MailService {
   options = {
     auth: {
-      api_key: "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",
+      api_key: email_api_key,
     },
   };
 
