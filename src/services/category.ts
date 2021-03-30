983b00e86b888dbd0565304461b991f2cbfb7b52
marciorasf <marciorasf@gmail.com>
2021-03-30 20:36:54 -0300

    fix expected behavior

--- src/services/category.ts
+++ src/services/category.ts
@@ -24,7 +24,7 @@ const categoryService = {
   },
 
   async retrieveOne(categoryId: number) {
-    return Category.find({
+    return Category.findOne({
       where: {
         id: categoryId,
       },
