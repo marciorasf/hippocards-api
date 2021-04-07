e88707749fb9e0eb3aaa5fb29d25312d4a35b228
marciorasf <marciorasf@gmail.com>
2021-04-06 21:40:49 -0300

    synchronize db only on dev

--- src/database.ts
+++ src/database.ts
@@ -2,12 +2,13 @@ import { createConnection } from "typeorm";
 import "reflect-metadata";
 
 import entities from "@/entities";
+import { __is_dev_env__ } from "@config/environment";
 import { __postgres_url__ } from "@config/postgres";
 
 createConnection({
   type: "postgres",
   url: __postgres_url__,
   entities,
-  synchronize: true,
-  logging: false,
+  synchronize: __is_dev_env__,
+  logging: __is_dev_env__,
 });
