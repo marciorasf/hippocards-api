423b572d5363a6e5bb32c98cfb6ef4191a36906c
marciorasf <marciorasf@gmail.com>
2021-04-07 21:36:52 -0300

    change database connection config

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,2 +1,3 @@
 export const __node_env__ = process.env.NODE_ENV;
 export const __is_dev_env__ = __node_env__ === "development";
+export const __is_prod_env__ = __node_env__ === "production";
--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,7 @@ import { createConnection } from "typeorm";
 import "reflect-metadata";
 
 import entities from "@/entities";
-import { __is_dev_env__ } from "@config/environment";
+import { __is_dev_env__, __is_prod_env__ } from "@config/environment";
 import { __postgres_url__ } from "@config/postgres";
 
 createConnection({
@@ -10,9 +10,9 @@ createConnection({
   url: __postgres_url__,
   entities,
   synchronize: __is_dev_env__,
-  logging: true,
+  logging: __is_prod_env__,
   extra: {
-    ssl: true,
+    ssl: __is_prod_env__,
     rejectUnauthorized: false,
   },
 });
