8b3df21877b9a347e362c7eae7b3eaec82fbd86c
marciorasf <marciorasf@gmail.com>
2021-04-07 21:14:12 -0300

    change database connection

--- src/database.ts
+++ src/database.ts
@@ -2,12 +2,16 @@ import { createConnection } from "typeorm";
 import "reflect-metadata";
 
 import entities from "@/entities";
+import { __is_dev_env__ } from "@config/environment";
 import { __postgres_url__ } from "@config/postgres";
 
 createConnection({
   type: "postgres",
   url: __postgres_url__,
   entities,
-  synchronize: true,
+  synchronize: __is_dev_env__,
   logging: true,
+  extra: {
+    ssl: true,
+  },
 });
