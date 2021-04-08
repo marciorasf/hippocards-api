75bffa08d4a0da4fb6c693f2c2d080692333fe17
marciorasf <marciorasf@gmail.com>
2021-04-07 23:04:44 -0300

    remove cookies

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,17 +1,18 @@
 import { Response, NextFunction, Request } from "express";
 import jwt from "jsonwebtoken";
 
-import { __cookies__ } from "@config/cookies";
 import { __secret__ } from "@config/encrypt";
 import ErrorService from "@services/error";
 import ResponseService from "@services/response";
 
 export default function authMiddleware(request: Request, response: Response, next: NextFunction) {
-  const token = request.cookies[__cookies__.auth.name];
+  const { authorization } = request.headers;
 
-  if (!token) {
+  if (!authorization) {
     ResponseService.unauthorized(response, { message: "unauthorized" });
   } else {
+    const token = authorization.replace("Bearer ", "");
+
     try {
       const decoded = jwt.verify(token, __secret__) as Record<"userId", number>;
       response.locals.userId = decoded.userId;
