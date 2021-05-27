2e0a01b676741f72040bc7af37f8bea6218ab286
marciorasf <marciorasf@gmail.com>
2021-05-26 21:15:51 -0300

    add client config

---
+++ src/config/SES.ts
@@ -0,0 +1,3 @@
+export const __ses_access_key_id__ = process.env.SES_ACCESS_KEY_ID || "";
+export const __ses_secret_access_key__ = process.env.SES_SECRET_ACCESS_KEY || "";
+export const __ses_region__ = process.env.SES_REGION;
---
+++ src/config/client.ts
@@ -0,0 +1,3 @@
+export const __client_url__ = process.env.CLIENT_URL;
+
+export const __client_change_password_path__ = process.env.CLIENT_CHANGE_PASSWORD_PATH;
